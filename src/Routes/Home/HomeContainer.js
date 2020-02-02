e98eb095b8dbb1c1542efc0e1f6a19a1b6388dac
realrlgus <realrlgus@naver.com>
2020-02-02 21:58:09 +0900

    Home Container End

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -13,8 +13,20 @@ export default class extends React.Component {
 
   async componentDidMount() {
     try {
-      const nowPlaying = await moviesApi.nowPlaying();
-      console.log(nowPlaying);
+      const {
+        data: { results: nowPlaying }
+      } = await moviesApi.nowPlaying();
+      const {
+        data: { results: upcoming }
+      } = await moviesApi.upcoming();
+      const {
+        data: { results: popular }
+      } = await moviesApi.popular();
+      this.setState({
+        nowPlaying,
+        upcoming,
+        popular
+      });
     } catch {
       this.setState({
         error: "Can't find movies information."
@@ -28,6 +40,7 @@ export default class extends React.Component {
 
   render() {
     const { nowPlaying, upcoming, popular, error, loading } = this.state;
+    console.log(this.state);
     return (
       <HomePresenter
         nowPlaying={nowPlaying}
--- src/api.js
+++ src/api.js
@@ -1,17 +1,20 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "https://api.themoviedb.org/3/",
+const config = {
   params: {
     api_key: "72bd8a884f26215e9f8e71bde34af65f",
     language: "ko-KR"
   }
+};
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3/"
 });
 
 export const moviesApi = {
-  nowPlaying: () => api.get("movie/now_playing"),
-  upComing: () => api.get("movie/upcoming"),
-  popular: () => api.get("movie/popular"),
+  nowPlaying: () => api.get("movie/now_playing", config),
+  upcoming: () => api.get("movie/upcoming", config),
+  popular: () => api.get("movie/popular", config),
   movieDetail: id =>
     api.get(`movie/${id}`, {
       params: {
@@ -21,24 +24,30 @@ export const moviesApi = {
   search: term =>
     api.get("search/movie", {
       params: {
+        api_key: "72bd8a884f26215e9f8e71bde34af65f",
+        language: "ko-KR",
         query: encodeURIComponent(term)
       }
     })
 };
 
 export const tvApi = {
-  topRated: () => api.get("tv/top_rated"),
-  popular: () => api.get("tv/popular"),
-  airingToday: () => api.get("tv/airing_today"),
+  topRated: () => api.get("tv/top_rated", config),
+  popular: () => api.get("tv/popular", config),
+  airingToday: () => api.get("tv/airing_today", config),
   showDetail: id =>
     api.get(`tv/${id}`, {
       params: {
+        api_key: "72bd8a884f26215e9f8e71bde34af65f",
+        language: "ko-KR",
         append_to_response: "videos"
       }
     }),
   search: term =>
     api.get("search/tv", {
       params: {
+        api_key: "72bd8a884f26215e9f8e71bde34af65f",
+        language: "ko-KR",
         query: encodeURIComponent(term)
       }
     })
